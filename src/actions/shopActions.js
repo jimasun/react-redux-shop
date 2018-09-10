3922291a6dcb9bd19d15bdbbd2ae7e6c8e2f9be4
Marius Jigoreanu <marius.jigoreanu@gmail.com>
2018-09-10 21:12:23 +0200

    Added more actions and separated files

--- src/actions/shopActions.js
+++ src/actions/shopActions.js
@@ -1,7 +1,9 @@
 import * as type from './index.js'
 
-export const addToCart = makeActionCreator(type.ADD_TO_CART, 'text')
-export const remFromCart = makeActionCreator(type.REM_FROM_CART, 'id', 'text')
+export const toggleInCart = makeActionCreator(type.TOGGLE_IN_CART, 'id')
+export const toggleInWhish = makeActionCreator(type.TOGGLE_IN_WHISH, 'id')
+export const addToCart = makeActionCreator(type.ADD_TO_CART, 'id')
+export const remFromCart = makeActionCreator(type.REM_FROM_CART, 'id')
 export const addToWhish = makeActionCreator(type.ADD_TO_WHISH, 'id')
 export const remFromWhish = makeActionCreator(type.REM_FROM_WHISH, 'id')
 
