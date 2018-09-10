748cef16be617e2f62cfde7b95adf921554bff82
Marius Jigoreanu <marius.jigoreanu@gmail.com>
2018-09-10 21:10:24 +0200

    Improved & fixed `shopReducer`
    * added initial state
    * added an implemented `toggoIn..` methods
    * corrected other reducer methods to shallow merge and return the `state` not only the interest item inthe reducer

--- src/reducers/shopReducer.js
+++ src/reducers/shopReducer.js
@@ -1,35 +1,75 @@
 import * as type from './../actions/index'
 
 const initialState = {
-  items: []
+  items: [],
+  cart: [],
+  whish: []
 }
 
-function addToCart(prevState, action) {
-  return {
+function toggleInCart(prevState, action) {
+  const items = prevState.items,
+    item = items.find(item => item.id == action.id),
+    index = items.indexOf(item)
+
+  !!item.inCart
+    ? remFromCart(prevState, action)
+    : addToCart(prevState, action)
+
+  item.inCart = !item.inCart
+  items.splice(index, 1, item)
+
+  return Object.assign(prevState, {
+    items: items,
+  })
+}
+
+function toggleInWhish(prevState, action) {
+  const items = prevState.items,
+    item = items.find(item => item.id == action.id),
+    index = items.indexOf(item)
+
+  !!item.inWhish
+    ? remFromWhish(prevState, action)
+    : addToWhish(prevState, action)
+
+  item.inWhish = !item.inWhish
+  items.splice(index, 1, item)
+
+  return Object.assign(prevState, {
+    items: items,
+  })
+}
+
+function addToCart(prevState, action) { 
+  return Object.assign(prevState, {
     cart: [...prevState.cart, action.id]
-  }
+  })
 }
 
 function remFromCart(prevState, action) {
-  return {
-    cart: [...provState.cart].splice(prevState.cart.indexOf(action.id))
-  }
+  return Object.assign(prevState, {
+    cart: [...prevState.cart].splice(prevState.cart.indexOf(action.id), 1)
+  })
 }
 
 function addToWhish(prevState, action) {
-  return {
+  return Object.assign(prevState, {
     whish: [...prevState.whish, action.id]
-  }
+  })
 }
 
 function remFromWhish(prevState, action) {
-  return {
-    whish: [...provState.whish].splice(prevState.whish.indexOf(action.id))
-  }
+  return Object.assign(prevState, {
+    whish: [...prevState.whish].splice(prevState.whish.indexOf(action.id), 1)
+  })
 }
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case type.TOGGLE_IN_CART:
+      return toggleInCart(state, action)
+    case type.TOGGLE_IN_WHISH:
+      return toggleInWhish(state, action)
     case type.ADD_TO_CART:
       return addToCart(state, action)
     case type.REM_FROM_CART:
