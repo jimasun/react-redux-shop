406dd5396fd9f3228b02accd1dbb02a53861ca92
Marius Jigoreanu <marius.jigoreanu@gmail.com>
2018-09-12 16:25:53 +0200

    INtegrated `Header` with the `Cart`

--- src/sites/Header.jsx
+++ src/sites/Header.jsx
@@ -1,17 +1,33 @@
 import React, { Component } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
+import Cart from './../comp/Cart'
+
 import './Header.css'
 
-const Header = () => {
-  return (
-    <div className="Header">
-      <div className="title">
-        <span className="icon"><FontAwesomeIcon icon="book" /></span>
-        React/ Redux Book Store</div>
-      <div className="cart"><FontAwesomeIcon icon="shopping-basket" /></div>
-    </div>
-  )
+class Header extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  toggleCart() {
+    document.querySelector('.Cart').classList.toggle('visible')
+  }
+
+  render() {
+    return (
+      <div className="Header">
+        <div className="title">
+          <span className="icon">
+            <FontAwesomeIcon icon="book" /></span>
+          React/ Redux Book Store</div>
+        <div className="cart"
+          onClick={this.toggleCart}>
+          <FontAwesomeIcon icon="shopping-basket" />
+          <Cart /></div>
+      </div>
+    )
+  }
 }
 
 export default Header
