19456c25220c513c32c511c3f0969b1c8e6851bb
Marius Jigoreanu <marius.jigoreanu@gmail.com>
2018-09-26 14:43:29 +0200

    Implemented `Wish`
    * `Wish` container now working
    * styling of `Wish` and `WishItem`
    * implementing `WishItem`

--- src/comp/Wish.js
+++ src/comp/Wish.js
@@ -1,14 +1,18 @@
 import { connect } from 'react-redux'
-import * as action from './../actions/wishActions'
+import * as wish from './../actions/wishActions'
+import * as cart from './../actions/cartActions'
 
 import Wish from './Wish/Wish'
 
 const mapStateToProps = (state) => ({
-  items: state.wish.items.reduce((acc, cur) => ({ ...acc, [cur.id]: cur }), {})
+  items: state.wish.items.map(itemId => Object.assign({},
+    state.shop.items.find(item => item.id === itemId)
+  )),
 })
 
 const mapDispatchToProps = (dispatch) => ({
-  removeFromWish: (id) => { dispatch(action.removeItem(id)) }
+  removeFromWish: (id) => { dispatch(wish.removeItem(id)) },
+  addToCart: (item) => dispatch(cart.addItem(item)),
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Wish)
--- src/comp/Wish/Wish.jsx
+++ src/comp/Wish/Wish.jsx
@@ -6,6 +6,22 @@ import './Wish.css'
 const Wish = (props) => {
   return (
     <div className="Wish">
+      <div className="header">
+        <span className="title">
+          You have
+          <span className="count"> {props.items.length} </span>
+          item(s) in your wishlist
+        </span>
+      </div>
+      <div className="items">
+        {props.items.map(item =>
+          <WishItem
+            key={`item-${item.id}`}
+            item={item}
+            removeFromWish={() => { props.removeFromWish(item) }}
+            addToCart={(item) => { props.addToCart(item) }} />
+        )}
+      </div>
     </div>
   )
 }
